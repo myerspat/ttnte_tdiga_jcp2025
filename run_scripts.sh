d904d21186e4bc2c90fa5acb5c43cc6ab1d5dff9
myerspat <myerspat@umich.edu>
2026-01-08 16:11:07 -0500

    update run script

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,7 +1,48 @@
 cd fixed_source/square
 python3 square.py | tee square.out
 python3 postprocess.py
-cd ../circle
+python3 plot_direction.py
+python3 plot_meshsize.py
+cd ../../
+
+cd fixed_source/circle
 python3 circle.py | tee circle.out
 python3 postprocess.py
+python3 plot_direction.py
+python3 plot_meshsize.py
+cd ../../
+
+cd fixed_source/quarter_circle
+python3 quarter_circle.py | tee quarter_circle.out
+python3 postprocess.py
+python3 plot_direction.py
+python3 plot_meshsize.py
+cd ../../
+
+cd fixed_source/cruciform
+python3 cruciform.py
+python3 plot.py
+cd ../../
+
+cd eigenvalue/quarter_circle
+python3 quarter_circle.py
+python3 plot.py
+cd ../../
+
+cd eigenvalue/circle
+python3 circle.py
+cd ../../
+
+cd eigenvalue/pincell
+python3 pincell.py
+python3 plot.py
+cd ../../
+
+cd eigenvalue/lightbridge_ba
+python3 lightbridge_ba.py
+python3 plot.py
+cd ../../
+
+cd eigenvalue/lightbridge_gas
+python3 lightbridge_gas.py
 cd ../../
